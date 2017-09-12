a55cdba384f0918a76825525c8c6e1e6accffb75
Zarni Hpyoe <zarnihpyoe@gmail.com>
2017-09-12 00:46:06 -0400

    algorithm: Implement generic quick sort

--- src/quick.rs
+++ src/quick.rs
@@ -1,29 +1,51 @@
-// time complexity -> O(nlogn)
+// extern crate rand;
 
-pub fn sort(vec: Vec<i32>) -> Vec<i32> {
-    if vec.len() < 2 {
-        return vec;
+// use self::rand::Rng;
+
+// average case time complexity -> O(nlogn)
+// worst case time complexity -> O(n^2)
+// unstable | inplace
+
+pub fn sort<T: PartialOrd + Copy>(xs: &mut Vec<T>) {
+    let len = xs.len();
+    if len < 2 {
+        return;
     }
-    // choose a pivot
-    // TODO: to choose pivot randomly
-    let pivot = &vec[0];
+    sub_routine(xs, 0, len - 1);
+}
+
+fn sub_routine<T: PartialOrd + Copy>(xs: &mut Vec<T>, l: usize, h: usize) {
+    // choose a random pivot
+    // let mut rng = rand::thread_rng();
+    // let pivot = xs[rng.gen_range(l, h+1)];
+    let pivot = xs[h];
+
+    // left and right pointers
+    let mut i = l;
+    let mut j = h;
 
     // partitioning
-    let mut smaller = vec![];
-    let mut larger = vec![];
-    for i in &vec[1..] {
-        match *i <= *pivot {
-            true => smaller.push(*i),
-            false => larger.push(*i),
+    while i <= j {
+        while xs[i] < pivot {
+            i += 1;
+        }
+        while xs[j] > pivot {
+            j -= 1;
+        }
+        if i <= j {
+            xs.swap(i, j);
+            i += 1;
+            j -= 1;
         }
     }
-
-    // combining altogether
-    let mut head = sort(smaller);
-    let mut tail = sort(larger);
-    head.push(*pivot);
-    head.append(&mut tail);
-    head
+    // left recursion
+    if l < j {
+        sub_routine(xs, l, j);
+    }
+    // right recursion
+    if h > i {
+        sub_routine(xs, i, h);
+    }
 }
 
 
@@ -33,32 +55,44 @@ mod tests {
 
     #[test]
     fn sort_empty_vec() {
-        let xs = vec![];
+        let mut xs = vec![];
+        sort(&mut xs);
         let e: Vec<i32> = vec![];
-        assert_eq!(e, sort(xs));
+        assert_eq!(e, xs);
     }
     #[test]
     fn sort_vec_with_one_item() {
-        let xs = vec![0];
+        let mut xs = vec![0];
+        sort(&mut xs);
         let e = vec![0];
-        assert_eq!(e, sort(xs));
+        assert_eq!(e, xs);
     }
     #[test]
     fn sort_vec_with_two_items() {
-        let xs = vec![1, 0];
+        let mut xs = vec![1, 0];
+        sort(&mut xs);
         let e = vec![0, 1];
-        assert_eq!(e, sort(xs));
+        assert_eq!(e, xs);
     }
     #[test]
     fn sort_vec_with_three_items() {
-        let xs = vec![2, 0, 1];
+        let mut xs = vec![2, 0, 1];
+        sort(&mut xs);
         let e = vec![0, 1, 2];
-        assert_eq!(e, sort(xs));
+        assert_eq!(e, xs);
     }
     #[test]
     fn sort_vec_with_multiple_items() {
-        let xs = vec![5, 2, 0, 1, 4, 3];
+        let mut xs = vec![5, 2, 0, 1, 4, 3];
+        sort(&mut xs);
         let e = vec![0, 1, 2, 3, 4, 5];
-        assert_eq!(e, sort(xs));
+        assert_eq!(e, xs);
+    }
+    #[test]
+    fn sort_vec_with_multiple_items_with_duplicates() {
+        let mut xs = vec![41, 32, 4, 56, 7, 45, 73, 60, 89, 8, 92];
+        sort(&mut xs);
+        let e = vec![4, 7, 8, 32, 41, 45, 56, 60, 73, 89, 92];
+        assert_eq!(e, xs);
     }
 }
--- tests/sort.rs
+++ tests/sort.rs
@@ -1,6 +1,6 @@
 extern crate dsa;
 
-use dsa::selection;
+use dsa::{selection, quick};
 
 #[test]
 fn selection_sort_integers_with_duplicates() {
@@ -25,4 +25,34 @@ fn selection_sort_strs_mix_lowercase_uppercase() {
     let mut xs = vec!["me", "You", "i", "theY", "them", "the"];
     let e = vec!["You", "i", "me", "the", "theY", "them"];
     assert_eq!(e, selection::sort(&mut xs));
+}
+
+
+#[test]
+fn quick_sort_integers_with_duplicates() {
+    let mut xs = vec![5, 5, 4, 2, 0, 3, 1, 2];
+    quick::sort(&mut xs);
+    let e = vec![0, 1, 2, 2, 3, 4, 5, 5];
+    assert_eq!(e, xs);
+}
+#[test]
+fn quick_sort_chars_with_duplicates() {
+    let mut xs = vec!['f', 'f', 'e', 'c', 'a', 'd', 'b', 'c'];
+    quick::sort(&mut xs);
+    let e = vec!['a', 'b', 'c', 'c', 'd', 'e', 'f', 'f'];
+    assert_eq!(e, xs);
+}
+#[test]
+fn quick_sort_strs_lowercase_only() {
+    let mut xs = vec!["me", "you", "i", "they", "them", "the"];
+    quick::sort(&mut xs);
+    let e = vec!["i", "me", "the", "them", "they", "you"];
+    assert_eq!(e, xs);
+}
+#[test]
+fn quick_sort_strs_mix_lowercase_uppercase() {
+    let mut xs = vec!["me", "You", "i", "theY", "them", "the"];
+    quick::sort(&mut xs);
+    let e = vec!["You", "i", "me", "the", "theY", "them"];
+    assert_eq!(e, xs);
 }
